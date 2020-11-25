52547164893f639b2d95e5ca1041040833fd05ec
shenghao.wang <shenghao.wang@go-jek.com>
2020-11-25 14:17:38 +0800

    Add script for outputting text

---
+++ 2.output_text/outputtext.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // flush: output the text without creating a line feed (carriage return)
+    // It doesn't go to a new line.
+    cout << "Starting program ..." << flush;
+
+    // endler will input a blank line and flush the output.
+    cout << "This is some text." << endl;
+    cout << "Banana. "
+         << "Apple. "
+         << "Orange. " << endl;
+    cout << "This is some more text" << endl;
+    return 0;
+}
