324d974b3f8368b32c4e5b718c821f99d9dc3598
shenghao.wang <shenghao.wang@go-jek.com>
2020-11-25 12:03:40 +0800

    Add hello world script

---
+++ 1.hello_world/helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // '<<': insertion operator
+    // 'endl': end line
+    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+    // 0 means normal
+    return 0;
+}
