950f6171011cb11ccc532b5ba8eaecb0b5b7cc48
shenghao.wang <shenghao.wang@go-jek.com>
2020-12-04 15:21:12 +0800

    Add tutorials for sizeof arrays

---
+++ 21.sizeof_multidimensional_arrays/sizeof-multidimensional-arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string animals[][3] = {{"fox", "dog", "cat"},
+                           {"mouse", "squirrel", "parrot"}};
+
+    cout << sizeof(string) << endl;  // 24
+    for (int i = 0; i < sizeof(animals) / sizeof(animals[0]); i++) {
+        for (int j = 0; j < sizeof(animals[0]) / sizeof(string); j++) {
+            cout << animals[i][j] << " " << flush;
+        }
+    }
+    return 0;
+}
